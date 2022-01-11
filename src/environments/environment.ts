85014e6d63c1571f3ecfa322dbb644ba2894babf
Aurelio Marín Bautista <72474524+vKurama7u7v@users.noreply.github.com>
2022-01-11 14:28:19 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'to-do-list-9ebfe',
-    appId: '1:910680274483:web:c2e114ab06c462cbba5546',
-    storageBucket: 'to-do-list-9ebfe.appspot.com',
-    apiKey: 'AIzaSyBS5JDpPwra6Ggg4grBsyd9RmHIdOi97UY',
-    authDomain: 'to-do-list-9ebfe.firebaseapp.com',
-    messagingSenderId: '910680274483',
+    projectId: 'XXXXXXXXXXXXXXXXXX',
+    appId: 'XXXXXXXXXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXXXXXXXXX',
+    apiKey: 'XXXXXXXXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXXXXXXXX',
+    messagingSenderId: 'XXXXXXXXXXXXXXXXXX',
   }
 
 };
